b6a0a2543f59028c343e3dce81746dd9b63d54bf
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-09 10:10:00 -0300

    Vou fazer deploy no render

--- back_end/api_back/server.js
+++ back_end/api_back/server.js
@@ -3,15 +3,26 @@ import express from "express";
 import cors from "cors";
 import rotaLogin from "../crud/rotas/rotaLogin.js";
 import rotaTask from "../crud/rotas/rotaTask.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log("__dirname", __dirname);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = 3000;
 
+// Rotas
 app.use("/api/login", rotaLogin);
-
 app.use("/api/tarefas", rotaTask);
+
+app.use(express.static(path.join(__dirname, "../../front_end/dist")));
+app.get("*any", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../front_end/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor está escutando na porta ${PORT}`);
