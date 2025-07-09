d4118406953e161530b94ab6700c1674be8aefe5
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-09 12:38:02 -0300

    Ajustando server.js

--- back_end/api_back/server.js
+++ back_end/api_back/server.js
@@ -1,3 +1,33 @@
+// import "dotenv/config";
+// import express from "express";
+// import cors from "cors";
+// import rotaLogin from "../crud/rotas/rotaLogin.js";
+// import rotaTask from "../crud/rotas/rotaTask.js";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// // console.log("__dirname", __dirname);
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// const PORT = 3000;
+
+// // Rotas
+// app.use("/api/login", rotaLogin);
+// app.use("/api/tarefas", rotaTask);
+
+// app.use(express.static(path.join(__dirname, "../../front_end/dist")));
+// app.get("*any", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../front_end/dist/index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Servidor está escutando na porta ${PORT}`);
+// });
+
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
@@ -8,21 +38,27 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// console.log("__dirname", __dirname);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = 3000;
 
-// Rotas
+const PORT = process.env.PORT || 3000;
+
+// Rotas de API
 app.use("/api/login", rotaLogin);
 app.use("/api/tarefas", rotaTask);
 
+// Servir arquivos do frontend
 app.use(express.static(path.join(__dirname, "../../front_end/dist")));
-app.get("*any", (req, res) => {
+
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../../front_end/dist/index.html"));
 });
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../front_end/dist/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Servidor está escutando na porta ${PORT}`);
