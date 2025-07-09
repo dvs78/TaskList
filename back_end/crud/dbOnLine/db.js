91b63b0bd5cbf597c8842d36d556ca225517fe19
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-09 13:26:24 -0300

    Corrigindo arquivo db.js

--- back_end/crud/dbOnLine/db.js
+++ back_end/crud/dbOnLine/db.js
@@ -1,13 +1,24 @@
+// import pg from "pg";
+
+// const pool = new pg.Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+
+// export default pool;
+
+// console.log((await pool.query("SELECT * FROM login")).rows);
+
 import pg from "pg";
 
 const pool = new pg.Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
-
-console.log((await pool.query("SELECT * FROM login")).rows);
