88df234a9153311d8fd113e0f796b181ad5c1cbd
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-11 17:46:18 -0300

    Checando5

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     target: "esnext", // para onde os arquivos serão exportados
+//   },
+// });
+
 export default defineConfig({
+  base: "/TaskList/",
   plugins: [react()],
   build: {
-    target: "esnext", // para onde os arquivos serão exportados
+    target: "esnext",
   },
 });
