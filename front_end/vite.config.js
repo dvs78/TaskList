4ca70f1f971416f6864705fc08903ec6dd8af003
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-11 18:25:27 -0300

    Corrige porta do servidor para funcionar no Render

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -9,9 +9,12 @@ import react from "@vitejs/plugin-react";
 //   },
 // });
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  base: "/TaskList/",
   plugins: [react()],
+  base: "./", // 👈 adiciona isso aqui
   build: {
     target: "esnext",
   },
