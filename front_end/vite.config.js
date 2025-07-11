97968ef054746badd67f153de2fa64139b50ac06
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-11 18:41:07 -0300

    Adiciona build corrigido com VITE_API_URL

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 // export default defineConfig({
@@ -12,10 +12,26 @@ import react from "@vitejs/plugin-react";
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "./", // 👈 adiciona isso aqui
-  build: {
-    target: "esnext",
-  },
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "./", // 👈 adiciona isso aqui
+//   build: {
+//     target: "esnext",
+//   },
+// });
+
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => {
+  // Carrega variáveis do .env baseado no modo (development ou production)
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [react()],
+    base: "./",
+    build: {
+      target: "esnext",
+    },
+  };
 });
