e9d37bb92060c194ecccd5d798e70748e5ff9969
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-12 14:35:58 -0300

    Conexão local e não local

--- back_end/api_back/connect.js
+++ back_end/api_back/connect.js
@@ -9,15 +9,45 @@
 // });
 
 // export default pool;
+// import pg from "pg";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const pool = new pg.Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false },
+// });
+
+// export default pool;
 import pg from "pg";
 import dotenv from "dotenv";
 
-dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
+const pool = new pg.Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+
+        // host: "localhost",
+        // port: 5432,
+        // user: "postgres",
+        // password: "Limao_10",
+        // database: "TaskList",
+      }
+);
+
+console.log("🌐 Ambiente:", process.env.NODE_ENV);
+console.log("🔗 Conectando com:", isProduction ? "Render/Remoto" : "Local");
 
 export default pool;
 
