a79d13ba6a2b053e30a88fc8a815a7a0d4af6a5e
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-12 14:49:51 -0300

    Conexão2 local e não local

--- back_end/api_back/connect.js
+++ back_end/api_back/connect.js
@@ -32,17 +32,11 @@ const pool = new pg.Pool(
         ssl: { rejectUnauthorized: false },
       }
     : {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-
-        // host: "localhost",
-        // port: 5432,
-        // user: "postgres",
-        // password: "Limao_10",
-        // database: "TaskList",
+        host: "localhost",
+        port: 5432,
+        user: "postgres",
+        password: "Limao_10",
+        database: "TaskList",
       }
 );
 
