297d08e4783f228d96a478973c80c2edc68f0ed7
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-12 15:58:35 -0300

    Conexão4 local e não local

--- build.js
+++
@@ -1,5 +0,0 @@
-// build.js
-import { execSync } from "child_process";
-
-execSync("npm install --prefix front_end", { stdio: "inherit" });
-execSync("npm run build --prefix front_end", { stdio: "inherit" });
