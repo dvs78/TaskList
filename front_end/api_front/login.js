79b2ed44b9bb9d8b4d593714bf4d8f4f3b27a4f2
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-12 08:13:46 -0300

    Aumentando input

--- front_end/api_front/login.js
+++ front_end/api_front/login.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 // const BASE_URL = "https://tasklist-g5wi.onrender.com";
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
+const BASE_URL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:3000";
 
 // Buscar todos os usuários
 export async function getLogin() {
--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -22,16 +22,20 @@
 
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig(({ mode }) => {
-  // Carrega variáveis do .env baseado no modo (development ou production)
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+export default ({ mode }) => {
+  // 👇 Carrega variáveis do .env conforme o modo (development ou production)
+  const env = loadEnv(mode, process.cwd());
 
-  return {
+  return defineConfig({
     plugins: [react()],
     base: "./",
+    define: {
+      "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL), // 👈 força embutir
+    },
     build: {
       target: "esnext",
     },
-  };
-});
+  });
+};
