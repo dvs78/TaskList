b5bcfaac774960db3e477ce16df362c8bd226eb0
Daniel Veiga <dvs.veiga78@gmail.com>
2025-07-31 20:58:41 -0300

    Voltando a comitar3

--- back_end/api_back/connect.js
+++ back_end/api_back/connect.js
@@ -1,24 +1,31 @@
 import pg from "pg";
 import dotenv from "dotenv";
 
-const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = process.env.NODE_ENV === "production";
 
-const pool = new pg.Pool(
-  isProduction
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-      }
-    : {
-        host: "localhost",
-        port: 5432,
-        user: "postgres",
-        password: "Limao_10",
-        database: "TaskList",
-      }
-);
+// const pool = new pg.Pool(
+//   isProduction
+//     ? {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: { rejectUnauthorized: false },
+//       }
+//     : {
+//         host: "localhost",
+//         port: 5432,
+//         user: "postgres",
+//         password: "Limao_10",
+//         database: "TaskList",
+//       }
+// );
 
-console.log("🌐 Ambiente:", process.env.NODE_ENV);
-console.log("🔗 Conectando com:", isProduction ? "Render/Remoto" : "Local");
+// console.log("🌐 Ambiente:", process.env.NODE_ENV);
+// console.log("🔗 Conectando com:", isProduction ? "Render/Remoto" : "Local");
+const pool = new pg.Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
 export default pool;
