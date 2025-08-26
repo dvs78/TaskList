00218ffb4dfcecb7a0b3d34e716fe6b6494ed220
Daniel Veiga <dvs.veiga78@gmail.com>
2025-08-26 16:13:25 -0300

    Salvando header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouseChimney } from "@fortawesome/free-solid-svg-icons";
+import { faCircleLeft } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <div>
-      Olá mundo
+    <header className="header">
+      <FontAwesomeIcon icon={faCircleLeft} />
+      <h1>Daniel</h1>
       <FontAwesomeIcon icon={faHouseChimney} />
-    </div>
+    </header>
   );
 };
 
