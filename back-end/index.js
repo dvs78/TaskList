b118f75bd97484485380921832a8be643d0e05c7
Daniel Veiga <dvs.veiga78@gmail.com>
2025-08-27 21:47:39 -0300

    Criação da requisição get

--- back-end/index.js
+++ back-end/index.js
@@ -1,3 +1,15 @@
 import express from "express";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Olá, mundo, estou aqui!");
+});
+
+app.post("/", (req, res) => {
+  res.send("Informação nova criada com sucesso!");
+});
+
+app.listen(3000, () => {
+  console.log("Meu servidor está rodando na porta https://localhost:3000");
+});
