62c0364991be994f5e013dd3afadc93d4f53f10a
Daniel Veiga <dvs.veiga78@gmail.com>
2025-08-29 11:53:06 -0300

    Requisição POST

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Olá, mundo, estou aqui!");
+  res.send("Olá, mundo, estou aqui agora!");
 });
 
 app.post("/", (req, res) => {
-  res.send("Informação nova criada com sucesso!");
+  const { body } = req; // o body é uma chave do objeto req
+  const { nome, idade } = body;
+  console.log(body); // vai aparecer no terminal
+  console.log(nome, idade); // vai aparecer no terminal
+  res.json({ nome, idade }); // vai aparecer no reqbin
 });
 
 app.listen(3000, () => {
