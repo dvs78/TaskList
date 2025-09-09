b57f9a9e1ceada4fc0f50581ab60bcb9ec06ff1b
Daniel Veiga <dvs.veiga78@gmail.com>
2025-09-09 13:02:39 -0300

    DATABASE_URL

--- backend/connect.js
+++ backend/connect.js
@@ -15,14 +15,11 @@
 // export default pool;
 
 import pkg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.DATABASE_URL_INTERNAL || process.env.DATABASE_URL_EXTERNAL,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
